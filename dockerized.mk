61ee14243d642842743be0121d6c03ff521840f8
Dan Li <danmuxingli@gmail.com>
2025-04-20 19:18:09 -0400

    feat: dockerization instructions

---
+++ dockerized.mk
@@ -0,0 +1,23 @@
+CC = gcc
+CFLAGS = -I./deps/libpg_query -I/usr/include -Wall -Wextra
+LDFLAGS = -L./deps/libpg_query -lpg_query -ljansson -lmicrohttpd
+
+# Main application
+pg_migration_graph.out: src/main.c src/parser.c src/graph_builder.c src/helper.c src/api.c
+	$(CC) $(CFLAGS) -o $@ $^ $(LDFLAGS)
+
+# Test binary
+test_parser.out: tests/test_parser.c src/parser.c src/helper.c
+	$(CC) $(CFLAGS) -o $@ $^ $(LDFLAGS)
+
+# Default target
+all: pg_migration_graph.out
+
+# Run tests
+test: test_parser.out
+	./test_parser.out
+	rm -f test_parser.out
+
+# Clean up
+clean:
+	rm -f pg_migration_graph.out test_parser.out
---
+++ src/api.h
@@ -0,0 +1,6 @@
+#ifndef API_H
+#define API_H
+
+#include <microhttpd.h>
+
+#endif
